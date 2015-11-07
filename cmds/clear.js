bcd6990cbd95680d9ba0fc85090c8248af439f01
cloudcome <cloudcome@163.com>
2015-11-07 12:33:03 +0800

    7niu

--- cmds/clear.js
+++ cmds/clear.js
@@ -7,8 +7,21 @@
 
 'use strict';
 
-module.exports = function () {
+var fse = require('fs-extra');
+var path = require('ydr-utils').path;
+var debug = require('ydr-utils').debug;
 
+module.exports = function (options) {
+    var destCacheFile = path.join(options.srcDirname, '7niu.cache.log');
+
+    try {
+        fse.removeSync(destCacheFile);
+        debug.success('clear cache', 'success');
+    } catch (err) {
+        debug.error('clear cache', path.toSystem(destCacheFile));
+        debug.error('clear cache', err.message);
+        return process.exit(1);
+    }
 };
 
 
