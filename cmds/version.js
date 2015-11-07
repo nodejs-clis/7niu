fcdfbc3ea6b2345ef822cf877d46caaeb52ab1fc
cloudcome <cloudcome@163.com>
2015-11-07 12:37:36 +0800

    7niu

--- cmds/version.js
+++ cmds/version.js
@@ -7,8 +7,22 @@
 
 'use strict';
 
-module.exports = function () {
+var npm = require('ydr-utils').npm;
+var debug = require('ydr-utils').debug;
 
+var pkg = require('../package.json');
+
+module.exports = function () {
+    debug.success('local version', pkg.version);
+    debug.ignore('check version', 'wait a moment...');
+    npm.getLatestVersion(pkg.name, function (err, version, json) {
+        if (err) {
+            debug.error('online version', err.message);
+            return process.exit(1);
+        }
+
+        debug.success('online version', version);
+    });
 };
 
 
