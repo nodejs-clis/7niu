5a14dfb88f715d3679a72dc71a9128750969b199
cloudcome <cloudcome@163.com>
2015-11-07 12:28:50 +0800

    7niu

--- cmds/init.js
+++ cmds/init.js
@@ -7,8 +7,47 @@
 
 'use strict';
 
-module.exports = function () {
+var fse = require('fs-extra');
+var path = require('ydr-utils').path;
+var typeis = require('ydr-utils').typeis;
+var debug = require('ydr-utils').debug;
 
+var json = {
+    access_key: 'your_access_key',
+    secret_key: 'your_secret_key',
+    bucket: 'your_bucket',
+    src: './',
+    upload: ['./*/**'],
+    dest: '/test/',
+    // 并行数量
+    parallel: 10,
+    contentType: 'application/octect-stream'
+};
+
+
+/**
+ * init 7niu.json
+ * @param options
+ * @returns {*}
+ */
+module.exports = function (options) {
+    var destJSONFile = path.join(options.srcDirname, '7niu.json');
+
+    if (typeis.file(destJSONFile)) {
+        debug.error('7niu.json', path.toSystem(destJSONFile) + ' is exist');
+        return process.exit(1);
+    }
+
+    try {
+        fse.outputFileSync(destJSONFile, JSON.stringify(json, null, 4));
+        debug.success('7niu.json', path.toSystem(destJSONFile));
+    } catch (err) {
+        debug.error('7niu.json', path.toSystem(destJSONFile));
+        debug.error('write file', err.message);
+        return process.exit(1);
+    }
+
+    return destJSONFile;
 };
 
 
